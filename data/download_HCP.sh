c4193b2a9bf94d637f26c3112777b3bd255b971d
Simon Dahan <57358392+SD3004@users.noreply.github.com>
2021-05-27 13:37:19 +0100

    Update download_HCP.sh

--- data/download_HCP.sh
+++ data/download_HCP.sh
@@ -1,6 +1,8 @@
 DBHOST=https://db.humanconnectome.org
 PROJECT=HCP_1200
 REST_URL_PREFIX=$DBHOST/data/archive/projects/$PROJECT/subjects
+PWD=0
+USER=0
 ##################################################################################################################
 #
 # GET a jsession
@@ -17,17 +19,16 @@ REST_URL_PREFIX=$DBHOST/data/archive/projects/$PROJECT/subjects
 #
 ##################################################################################################################
 
-jsession='curl $MY_CURL_OPTIONS -u ${username}:$db_password ${pwd}'
+jsession='curl $MY_CURL_OPTIONS -u ${USER}:$db_password ${PWD}'
  
 while read line
 do
     subject=$line
     subject_url_prefix=$REST_URL_PREFIX/$subject/experiments/${subject}_CREST/resources/${subject}_CREST/files
-    #file_relative_path=MNINonLinear/Results/rfMRI_REST1_LR/rfMRI_REST1_LR_Atlas_MSMAll.dtseries.nii
-    #file_relative_path=MNINonLinear/Results/rfMRI_REST2_LR/rfMRI_REST2_LR_Atlas_MSMAll.dtseries.nii
+    
     file_relative_path=MNINonLinear/Results/rfMRI_REST1_LR/rfMRI_REST1_LR_Atlas_hp2000_clean.dtseries.nii
+    
     echo $subject_url_prefix/$file_relative_path 
-    #curl -u sd3004:11@Raphaella30 -o /data/HCP/rfMRI/rfMRI_REST1_LR/${subject}_rfMRI_REST1_LR_Atlas_MSMAll.dtseries.nii --create-dir $subject_url_prefix/$file_relative_path
-    #curl -u sd3004:11@Raphaella30 -o /data/HCP/rfMRI/rfMRI_REST2_LR/${subject}_rfMRI_REST1_LR_Atlas_MSMAll.dtseries.nii --create-dir $subject_url_prefix/$file_relative_path
-    curl -u sd3004:11@Raphaella30 -o /data/HCP/rfMRI/test/${subject}_rfMRI_REST1_LR_Atlas_hp2000_clean.dtseries.nii --create-dir $subject_url_prefix/$file_relative_path
+    
+    curl -u {USER}:{PWD} -o /data/HCP/rfMRI/${subject}_rfMRI_REST1_LR_Atlas_hp2000_clean.dtseries.nii --create-dir $subject_url_prefix/$file_relative_path
 done <HCP_subject.csv
